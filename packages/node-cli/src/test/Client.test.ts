71a6ad60d8425bcbb3dcc675754e1e851efafee7
mgooding <869053+mgooding@users.noreply.github.com>
2022-04-06 17:11:03 -0400

    Add simple command-line authentication client for devtools (#69)
    Co-authored-by: Caleb Shafer <31107829+calebmshafer@users.noreply.github.com>

---
+++ packages/node-cli/src/test/Client.test.ts
@@ -0,0 +1,81 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+
+import * as chai from "chai";
+import * as chaiAsPromised from "chai-as-promised";
+
+import { BakedAuthorizationConfiguration } from "../Client";
+import type { NodeCliAuthorizationConfiguration } from "../Client";
+
+chai.use(chaiAsPromised);
+
+describe("NodeCliAuthorizationConfiguration defaults", () => {
+  it("should throw if clientId is an empty string", () => {
+    chai.expect(() => new BakedAuthorizationConfiguration({ clientId: "", scope: "testScope" })).to.throw();
+  });
+
+  it("should throw if scope is an empty string", () => {
+    chai.expect(() => new BakedAuthorizationConfiguration({ clientId: "testClientId", scope: "" })).to.throw();
+  });
+});
+
+describe("NodeCliAuthorizationConfiguration Authority URL Logic", () => {
+  const config: NodeCliAuthorizationConfiguration = {
+    clientId: "testClientId",
+    scope: "testScope",
+  };
+  const testAuthority = "https://test.authority.com";
+
+  it("should use config authority without prefix", async () => {
+    process.env.IMJS_URL_PREFIX = "";
+    const bakedConfig = new BakedAuthorizationConfiguration({ ...config, issuerUrl: testAuthority });
+    chai.expect(bakedConfig.issuerUrl).equals(testAuthority);
+  });
+
+  it("should use config authority and ignore prefix", async () => {
+    process.env.IMJS_URL_PREFIX = "prefix-";
+    const bakedConfig = new BakedAuthorizationConfiguration({ ...config, issuerUrl: testAuthority });
+    chai.expect(bakedConfig.issuerUrl).equals("https://test.authority.com");
+  });
+
+  it("should use default authority without prefix ", async () => {
+    process.env.IMJS_URL_PREFIX = "";
+    const bakedConfig = new BakedAuthorizationConfiguration(config);
+    chai.expect(bakedConfig.issuerUrl).equals("https://ims.bentley.com");
+  });
+
+  it("should use default authority with prefix ", async () => {
+    process.env.IMJS_URL_PREFIX = "prefix-";
+    const bakedConfig = new BakedAuthorizationConfiguration(config);
+    chai.expect(bakedConfig.issuerUrl).equals("https://prefix-ims.bentley.com");
+  });
+
+  it("should reroute dev prefix to qa if on default ", async () => {
+    process.env.IMJS_URL_PREFIX = "dev-";
+    const bakedConfig = new BakedAuthorizationConfiguration(config);
+    chai.expect(bakedConfig.issuerUrl).equals("https://qa-ims.bentley.com");
+  });
+});
+
+describe("NodeCliAuthorizationConfiguration Config Scope Logic", () => {
+  const config: NodeCliAuthorizationConfiguration = {
+    clientId: "testClientId",
+    scope: "testScope",
+  };
+
+  it("Should add offline_access scope", async () => {
+    const bakedConfig = new BakedAuthorizationConfiguration(config);
+    chai.expect(bakedConfig.scopes).equals(`${config.scope} offline_access`);
+  });
+
+  it("Should not add offline_access scope", async () => {
+    const bakedConfig = new BakedAuthorizationConfiguration({
+      clientId: "testClientId",
+      scope: "testScope offline_access",
+    });
+    chai.expect(bakedConfig.scopes).equals("testScope offline_access");
+  });
+});
+
