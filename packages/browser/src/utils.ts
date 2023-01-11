6356f80540ed9b2ba1e84d40651750949d55a938
Matthew Jordan <14913576+mattbjordan@users.noreply.github.com>
2023-01-11 16:11:57 -0500

    add copyright header

--- packages/browser/src/utils.ts
+++ packages/browser/src/utils.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+
 const imsUrl = "https://ims.bentley.com";
 
 /**
