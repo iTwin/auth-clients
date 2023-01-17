b871e92ac63660fee56ac7ba3841327e4c826994
Matthew Jordan <14913576+mattbjordan@users.noreply.github.com>
2023-01-17 11:31:39 -0500

    group authority prefix tests together

--- packages/browser/src/test/BrowserAuthorizationClient.test.ts
+++ packages/browser/src/test/BrowserAuthorizationClient.test.ts
@@ -7,42 +7,46 @@ import * as chai from "chai";
 import type { BrowserAuthorizationClientConfiguration } from "../Client";
 import { BrowserAuthorizationClient } from "../Client";
 
+
 describe("BrowserAuthorizationClient", () => {
 
-  const browserConfiguration: BrowserAuthorizationClientConfiguration = {
-    clientId: "testClientId",
-    redirectUri: "testClientSecret",
-    scope: "testScope",
-  };
-  const testAuthority = "https://test.authority.com";
+  describe("authority has correct prefix", () => {
 
-  it("should use config authority without prefix", async () => {
-    process.env.IMJS_URL_PREFIX = "";
-    const client = new BrowserAuthorizationClient({ ...browserConfiguration, authority: testAuthority });
-    chai.expect(client.getAuthorityUrl()).equals(testAuthority);
-  });
+    const browserConfiguration: BrowserAuthorizationClientConfiguration = {
+      clientId: "testClientId",
+      redirectUri: "testClientSecret",
+      scope: "testScope",
+    };
+    const testAuthority = "https://test.authority.com";
 
-  it("should use config authority and ignore prefix", async () => {
-    process.env.IMJS_URL_PREFIX = "prefix-";
-    const client = new BrowserAuthorizationClient({ ...browserConfiguration, authority: testAuthority });
-    chai.expect(client.getAuthorityUrl()).equals("https://test.authority.com");
-  });
+    it("should use config authority without prefix", async () => {
+      process.env.IMJS_URL_PREFIX = "";
+      const client = new BrowserAuthorizationClient({ ...browserConfiguration, authority: testAuthority });
+      chai.expect(client.getAuthorityUrl()).equals(testAuthority);
+    });
 
-  it("should use default authority without prefix ", async () => {
-    process.env.IMJS_URL_PREFIX = "";
-    const client = new BrowserAuthorizationClient(browserConfiguration);
-    chai.expect(client.getAuthorityUrl()).equals("https://ims.bentley.com");
-  });
+    it("should use config authority and ignore prefix", async () => {
+      process.env.IMJS_URL_PREFIX = "prefix-";
+      const client = new BrowserAuthorizationClient({ ...browserConfiguration, authority: testAuthority });
+      chai.expect(client.getAuthorityUrl()).equals("https://test.authority.com");
+    });
 
-  it("should use default authority with prefix ", async () => {
-    process.env.IMJS_URL_PREFIX = "prefix-";
-    const client = new BrowserAuthorizationClient(browserConfiguration);
-    chai.expect(client.getAuthorityUrl()).equals("https://prefix-ims.bentley.com");
-  });
+    it("should use default authority without prefix ", async () => {
+      process.env.IMJS_URL_PREFIX = "";
+      const client = new BrowserAuthorizationClient(browserConfiguration);
+      chai.expect(client.getAuthorityUrl()).equals("https://ims.bentley.com");
+    });
 
-  it("should reroute dev prefix to qa if on default ", async () => {
-    process.env.IMJS_URL_PREFIX = "dev-";
-    const client = new BrowserAuthorizationClient(browserConfiguration);
-    chai.expect(client.getAuthorityUrl()).equals("https://qa-ims.bentley.com");
+    it("should use default authority with prefix ", async () => {
+      process.env.IMJS_URL_PREFIX = "prefix-";
+      const client = new BrowserAuthorizationClient(browserConfiguration);
+      chai.expect(client.getAuthorityUrl()).equals("https://prefix-ims.bentley.com");
+    });
+
+    it("should reroute dev prefix to qa if on default ", async () => {
+      process.env.IMJS_URL_PREFIX = "dev-";
+      const client = new BrowserAuthorizationClient(browserConfiguration);
+      chai.expect(client.getAuthorityUrl()).equals("https://qa-ims.bentley.com");
+    });
   });
 });
