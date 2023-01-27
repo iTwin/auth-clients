e8d420e82da92747c9bb92956b55e02121bb7e91
Evelyn Preslar <54330647+evelynpreslar-bentley@users.noreply.github.com>
2023-01-27 13:44:29 -0500

    Update browser authorization to oidc-client-ts (#88)
    Co-authored-by: Caleb Shafer <31107829+calebmshafer@users.noreply.github.com>
    Co-authored-by: Matthew Jordan <14913576+mattbjordan@users.noreply.github.com>

---
+++ packages/browser/src/test/utils.test.ts
@@ -0,0 +1,34 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+
+import { assert } from "chai";
+import { getImsAuthority } from "../utils";
+
+describe("getImsAuthority", () => {
+
+  it("has no prefix when no prefix is defined", async () => {
+    process.env.IMJS_URL_PREFIX = "";
+
+    assert.equal(getImsAuthority(), "https://ims.bentley.com");
+  });
+
+  it("has correct prefix when prefix is defined", async () => {
+    process.env.IMJS_URL_PREFIX = "prefix-";
+
+    assert.equal(getImsAuthority(), "https://prefix-ims.bentley.com");
+  });
+
+  it("replaces \"dev-\" prefix with \"qa-\"", async () => {
+    process.env.IMJS_URL_PREFIX = "dev-";
+
+    assert.equal(getImsAuthority(), "https://qa-ims.bentley.com");
+  });
+
+  it("does not modify \"qa-\" prefix", async () => {
+    process.env.IMJS_URL_PREFIX = "qa-";
+
+    assert.equal(getImsAuthority(), "https://qa-ims.bentley.com");
+  });
+});
---
+++ packages/browser/src/utils.ts
@@ -0,0 +1,22 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+
+/**
+ * Returns the IMS authority URL.
+ * A prefix will be prepended based on the value of the IMJS_URL_PREFIX environment variable.
+ * The prefix "dev-" will automatically be converted to "qa-".
+ * @param authorityUrl
+ * @returns
+ */
+export function getImsAuthority(): string {
+
+  let prefix = process.env.IMJS_URL_PREFIX ?? "";
+
+  // "dev-ims.bentley.com" doesn't exist, so convert prefix to "qa-"
+  if (prefix === "dev-")
+    prefix = "qa-";
+
+  return `https://${prefix}ims.bentley.com`;
+}
