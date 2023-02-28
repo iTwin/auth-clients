31a2a2f000e8357fb32b48bca1f71fcf1f382972
Ben Polinsky <benpolinsky@users.noreply.github.com>
2023-02-28 10:23:10 -0500

    add callback handler for test

--- packages/browser/src/integration-tests/test-app/index.ts
+++ packages/browser/src/integration-tests/test-app/index.ts
@@ -1,9 +1,13 @@
-import { BrowserAuthorizationClient } from "../../index";
+import {
+  BrowserAuthorizationClient,
+  BrowserAuthorizationCallbackHandler,
+  BrowserAuthorizationCallbackHandlerConfiguration,
+} from "../../index";
 
 const signInAuthButton = document.querySelector("button#signIn");
 
 const client = new BrowserAuthorizationClient({
-  clientId: "",
+  clientId: "spa-QRTqE2Iq37h7vflXvTM2poAbr",
   redirectUri: "http://localhost:1234/oidc-callback",
   scope: "itwins:read projects:read itwins:modify projects:modify users:read",
   authority: "https://ims.bentley.com",
@@ -14,8 +18,21 @@ const client = new BrowserAuthorizationClient({
 
 if (signInAuthButton) {
   signInAuthButton.addEventListener("click", () => {
-    client.signInPopup().then(() => {
+    client.signInRedirect().then(() => {
       console.log("signInPopup triggered");
     });
+  });
+}
+
+if (window.location.href.includes("oidc-callback")) {
+  const callbackHandler =
+    BrowserAuthorizationCallbackHandler.handleSigninCallback({
+      clientId: "spa-QRTqE2Iq37h7vflXvTM2poAbr",
+      redirectUri: "http://localhost:1234/oidc-callback",
+      authority: "https://ims.bentley.com",
+      responseMode: "query",
+    });
+  callbackHandler.then(() => {
+    console.log("done?");
   });
 }
