afca90df37350c2d348f313df6527498fa725867
Ben Polinsky <ben-polinsky@users.noreply.github.com>
2023-03-03 15:34:52 -0500

    fix playwright reporter types

--- packages/browser/playwright.config.ts
+++ packages/browser/playwright.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "@playwright/test";
 
+// playwright types here are... not cooperating
 const reporter: any = process.env.AGENT_ID
-  ? [["junit", { outputFile: "junit_results.xml" }], "dot"]
+  ? [
+      ["junit", { outputFile: "junit_results.xml" }],
+      ["dot", []],
+    ]
   : "list";
 
 export default defineConfig({
