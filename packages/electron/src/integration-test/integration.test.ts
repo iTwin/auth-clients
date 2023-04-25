d171c06ac7ef76e6c036989e4a7c2e38edd8224e
maggiemah <55515768+maggiemah@users.noreply.github.com>
2023-04-24 18:45:21 -0700

    Add copyrights

--- packages/electron/src/integration-test/integration.test.ts
+++ packages/electron/src/integration-test/integration.test.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+
 import { ElectronApplication, Page, expect, test } from "@playwright/test";
 import { SignInOptions } from "./types";
 import { loadConfig } from "./helpers/loadConfig";
