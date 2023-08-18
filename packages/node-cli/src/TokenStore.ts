67e1402d896fe615128320d1d906a003a8bf2451
Hoang Nam Le <50554904+hl662@users.noreply.github.com>
2023-08-18 17:03:17 -0400

    Migrate from node-keytar to crypto

--- packages/node-cli/src/TokenStore.ts
+++ packages/node-cli/src/TokenStore.ts
@@ -3,9 +3,11 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 
-import { getPassword, setPassword } from "keytar";
 import * as OperatingSystemUserName from "username";
 import { TokenResponse } from "@openid/appauth";
+import { createCipheriv, createDecipheriv, randomBytes, scryptSync } from "crypto";
+import * as path from "node:path";
+import * as NodePersist from "node-persist";
 
 /**
  * Utility to store OIDC AppAuth in secure storage
@@ -14,21 +16,63 @@ import { TokenResponse } from "@openid/appauth";
 export class TokenStore {
   private readonly _appStorageKey: string;
   private readonly _scopes: string;
-
-  public constructor(namedArgs: {clientId: string, issuerUrl: string, scopes: string}) {
+  private readonly _store: NodePersist.LocalStorage;
+  public constructor(namedArgs: {clientId: string, issuerUrl: string, scopes: string}, dir?: string) {
     // A stored credential is only valid for a combination of the clientId, the issuing authority and the requested scopes.
     // We make the storage key a combination of clientId and issuing authority so that keys can stay cached when switching
     // between PROD and QA environments.
     // We store the scopes in our password blob so we know if a new token is required due to updated scopes.
-    this._appStorageKey = `iTwinJs:${namedArgs.clientId}:${namedArgs.issuerUrl}`;
+    const configFileName = `itwinJs_${namedArgs.clientId}`;
+    this._appStorageKey = `${configFileName}_${namedArgs.issuerUrl}`
+      .replace(/[.]/g, "%2E") // Replace all '.' with URL Percent-encoding representation
+      .replace(/[\/]/g, "%2F"); // Replace all '/' with URL Percent-encoding representation;
     this._scopes = namedArgs.scopes;
+    this._store = NodePersist.create({
+      dir: dir ?? path.join(process.cwd(), ".configStore"), // specifies storage file name.
+    });
   }
 
+  public async initialize(): Promise<void> {
+    await this._store.init();
+  }
   private _userName?: string;
   private async getUserName(): Promise<string | undefined> {
     if (!this._userName)
       this._userName = await OperatingSystemUserName();
     return this._userName;
+  }
+
+  private async getKey(): Promise<string> {
+    const userName = await this.getUserName();
+    return `${this._appStorageKey}${userName}`;
+  }
+
+  /**
+   * Generate a viable cipher key from a password based derivation function (scrypt).
+   * @returns
+   */
+  private generateCipherKey(): Buffer {
+    return scryptSync(this._appStorageKey, "iTwin", 32); // aes-256-cbc requires a key length of 32 bytes.
+  }
+  /**
+   * Uses node's native `crypto` module to encrypt the given cache entry.
+   * @returns an object containing a hexadecimal encoded token, returned as a string, as well as the initialization vector.
+   */
+  private encryptCache(cacheEntry: object): {encryptedCache: string, iv: string} {
+    const iv = randomBytes(16);
+    const cipher = createCipheriv("aes-256-cbc", this.generateCipherKey(), iv);
+
+    const encryptedCache = cipher.update(JSON.stringify(cacheEntry), "utf8", "hex") + cipher.final("hex");
+    return {
+      encryptedCache,
+      iv: iv.toString("hex"),
+    };
+  }
+
+  private decryptCache(encryptedCache: string, iv: Buffer): string {
+    const decipher = createDecipheriv("aes-256-cbc", this.generateCipherKey(), iv);
+    const decryptedCache = decipher.update(encryptedCache, "hex", "utf8") + decipher.final("utf8");
+    return decryptedCache;
   }
 
   public async load(): Promise<TokenResponse | undefined> {
@@ -37,14 +81,19 @@ export class TokenStore {
 
     const userName = await this.getUserName();
     if (!userName)
-      return;
-
-    const tokenResponseStr = await getPassword(this._appStorageKey, userName);
-    if (!tokenResponseStr)
       return undefined;
 
+    const key = await this.getKey();
+    const storeKeys = await this._store.keys();
+    if (!storeKeys.includes(key)) {
+      return undefined;
+    }
+    const storedObj = await this._store.getItem(key);
+    const encryptedCache = storedObj.encryptedCache;
+    const iv = storedObj.iv;
+    const cacheEntry = this.decryptCache(encryptedCache, Buffer.from(iv, "hex"));
     // Only reuse token if matching scopes. Don't include cache data for TokenResponse object.
-    const tokenResponseObj = JSON.parse(tokenResponseStr);
+    const tokenResponseObj = JSON.parse(cacheEntry);
     if (tokenResponseObj?.scopesForCacheValidation !== this._scopes)
       return undefined;
     delete tokenResponseObj.scopesForCacheValidation;
@@ -69,7 +118,8 @@ export class TokenStore {
       scopesForCacheValidation: this._scopes,
       ...tokenResponseObj.toJson(),
     };
-
-    await setPassword(this._appStorageKey, userName, JSON.stringify(cacheEntry));
+    const objToStore = this.encryptCache(cacheEntry);
+    const key = await this.getKey();
+    await this._store.setItem(key, objToStore);
   }
 }
---
+++ packages/node-cli/src/test/TokenStore.test.ts
@@ -0,0 +1,48 @@
+import { TokenResponse } from "@openid/appauth";
+import * as chai from "chai";
+import * as chaiAsPromised from "chai-as-promised";
+import { TokenStore } from "../TokenStore";
+import { rmSync } from "fs";
+import * as sinon from "sinon";
+chai.use(chaiAsPromised);
+
+describe("TokenStore", () => {
+  let tokenStore: TokenStore;
+  const testTokenResponse = new TokenResponse({
+    access_token: "testAccessToken", // eslint-disable-line @typescript-eslint/naming-convention
+    refresh_token: "testRefreshToken", // eslint-disable-line @typescript-eslint/naming-convention
+    scope: "scope1 scope2",
+  });
+
+  beforeEach(async () => {
+    tokenStore = new TokenStore({
+      clientId: "testClientId",
+      issuerUrl: "https://testUrl.com",
+      scopes: "testScope1 testScope2",
+    }, `${process.cwd()}/testConfig`);
+    await tokenStore.initialize();
+  });
+
+  afterEach(() => {
+    rmSync(`${process.cwd()}/testConfig`, {recursive: true, force: true});
+  });
+
+  it("should encrypt cache on save", async () => {
+    if (process.platform === "linux")
+      return;
+
+    const saveSpy = sinon.spy(tokenStore as any, "encryptCache");
+    await tokenStore.save(testTokenResponse);
+    chai.assert.isTrue(saveSpy.calledOnce);
+  });
+
+  it("should decrypt cache on load", async () => {
+    if (process.platform === "linux")
+      return;
+
+    await tokenStore.save(testTokenResponse);
+
+    const retrievedToken = await tokenStore.load();
+    chai.expect(retrievedToken!.refreshToken).equals(testTokenResponse.refreshToken);
+  });
+});
