7cb7082fbd8e1367be4e76b617b0486a8d261ef6
Michael Belousov <michael.belousov98@gmail.com>
2023-10-26 15:39:02 -0400

    more generic utility-process capable automation

--- packages/oidc-signin-tool/src/loginElectronMainAuthClient.ts
+++ packages/oidc-signin-tool/src/loginElectronMainAuthClient.ts
@@ -2,10 +2,12 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-import type { TestUserCredentials, } from "./TestUsers";
+import type { TestUserCredentials } from "./TestUsers";
 import * as SignInAutomation from "./SignInAutomation";
-import type { ElectronApplication, JSHandle } from "@playwright/test";
-import * as assert from "node:assert";
+import type { ElectronApplication, JSHandle, Page } from "@playwright/test";
+
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+type Electron = typeof import("electron");
 
 /**
  * playwright (and other async contexts) will eagerly evaluate returned promises
@@ -20,9 +22,25 @@ async function promiseHandleValue<T>(promiseHandle: PromiseHandle<T>): Promise<T
 }
 
 /**
+ * helper type to allow non-ElectronApplication contexts
+ * e.g. a thirdparty playwright API implementation over an electron utility process
+ * @alpha
+ */
+export interface PlaywrightElectronContext {
+  evaluate<T, R>(
+    func: (electron: Electron, passed?: T) => R,
+    args?: T
+  ): Promise<R>;
+  evaluateHandle<T, R>(
+    func: (electron: Electron, passed?: T) => R,
+    args?: T
+  ): Promise<JSHandle<R>>;
+}
+
+/**
  * returns a handle to a pending promise that resolves the next time electron.shell.openExternal is called
  */
-async function setupGetNextFetchedUrl(app: ElectronApplication): Promise<PromiseHandle<string>> {
+async function setupGetNextFetchedUrl(app: PlaywrightElectronContext): Promise<PromiseHandle<string>> {
   return app.evaluateHandle(({ shell }) => {
     return {
       promise: new Promise<string>((resolve) => {
@@ -37,9 +55,9 @@ async function setupGetNextFetchedUrl(app: ElectronApplication): Promise<Promise
   });
 }
 
+// FIXME: used wrong indent setting everywhere!
 async function getExtraWindowAsBrowserFromElectron(app: ElectronApplication) {
-  const prevWindows = app.windows();
-  const prevWindowCount = prevWindows.length;
+  const newWindowPromise = new Promise<Page>((resolve) => app.on("window", resolve));
 
   await app.evaluate(
     // eslint-disable-next-line @typescript-eslint/naming-convention
@@ -52,27 +70,36 @@ async function getExtraWindowAsBrowserFromElectron(app: ElectronApplication) {
     },
   );
 
-  const nowWindows = app.windows();
-  assert(prevWindowCount === nowWindows.length - 1);
-  const loginWindow = nowWindows[nowWindows.length - 1];
-
-  return loginWindow;
+  return newWindowPromise;
 }
 
 /**
- * given an electron app, user, auth config, and a callback to start signing in,
- * complete the log in flow as that user.
+ * given an electron app, auth configuration, and a callback to start signing in,
+ * complete the log in flow as the configured user.
  * @alpha
  */
 export async function loginElectronMainAuthClient(
-  app: ElectronApplication,
-  /** any function that initiates ElectronMainAuthorization.signIn() */
-  startSignIn: () => Promise<void>,
-  user: TestUserCredentials,
-  config: SignInAutomation.AutomatedSignInConfig,
-  loginBrowser: "chromium" | "separate-electron-window" = "chromium",
+  {
+    app,
+    backendContext = app,
+    startSignIn,
+    user,
+    config,
+    loginBrowser = "chromium",
+  }: {
+    app: ElectronApplication;
+    /** the playwright context which holds iTwin.js backend code.
+     * @default the passed in app, since that is usually where backend code is run
+     */
+    backendContext?: PlaywrightElectronContext | ElectronApplication;
+    /** any function that eventually invokes ElectronMainAuthorization.signIn() */
+    startSignIn: () => Promise<void>;
+    user: TestUserCredentials;
+    config: SignInAutomation.AutomatedSignInConfig;
+    loginBrowser?: "chromium" | "separate-electron-window";
+  }
 ) {
-  const nextFetchedUrlPromise = await setupGetNextFetchedUrl(app);
+  const nextFetchedUrlPromise = await setupGetNextFetchedUrl(backendContext);
   void startSignIn();
   const requestedLoginUrl = await promiseHandleValue(nextFetchedUrlPromise);
   if (!requestedLoginUrl)
