254f3a5d08bf205a3360639cd1e5ffa72565b4dd
Gintaras Volkvičius <17436829+GintV@users.noreply.github.com>
2024-03-28 11:09:46 +0200

    Unify expiry buffer accross electron auth clients (#237)

---
+++ packages/electron/src/common/constants.ts
@@ -0,0 +1,9 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+
+/**
+ * Refresh token 10 minutes before real expiration time (by default)
+ */
+export const defaultExpiryBufferInSeconds = 600;
