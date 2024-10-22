945cad7b9b1c947d12485baead08204fc2454c32
Ben Polinsky <ben-polinsky@users.noreply.github.com>
2024-10-22 15:05:46 -0400

    unit tests do need to build

--- packages/oidc-signin-tool/eslint.config.js
+++ packages/oidc-signin-tool/eslint.config.js
@@ -3,17 +3,17 @@ const iTwinPlugin = require("@itwin/eslint-plugin");
 module.exports = [
   {
     files: ["**/*.{ts,tsx}"],
-    ignores: ["**/test-integration/**", "**/test/**"],
+    ignores: ["**/test-integration/**"],
     ...iTwinPlugin.configs.iTwinjsRecommendedConfig,
   },
   {
     files: ["**/*.{ts,tsx}"],
-    ignores: ["**/test-integration/**", "**/test/**"],
+    ignores: ["**/test-integration/**"],
     ...iTwinPlugin.configs.jsdocConfig,
   },
   {
     files: ["**/*.{ts,tsx}"],
-    ignores: ["**/test-integration/**", "**/test/**"],
+    ignores: ["**/test-integration/**"],
     rules: {
       "no-duplicate-imports": "off",
       "@typescript-eslint/consistent-type-imports": "error",
