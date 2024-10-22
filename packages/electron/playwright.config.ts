9578ae6205fee48df5789dafb87d858c22447b13
Ben Polinsky <ben-polinsky@users.noreply.github.com>
2024-10-22 10:27:56 -0400

    downgrade playwright

--- packages/electron/playwright.config.ts
+++ packages/electron/playwright.config.ts
@@ -7,7 +7,7 @@ import { PlaywrightTestConfig } from '@playwright/test'
 
 const config: PlaywrightTestConfig = {
   testDir: './src/integration-test',
-  timeout: 60000,
+  timeout: 5000,
   use: {
     screenshot: "only-on-failure",
     headless: true
