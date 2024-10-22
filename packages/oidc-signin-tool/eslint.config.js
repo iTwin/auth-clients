6ccc4d7a5c391c282d8846fe09587c30666ab256
Ben Polinsky <ben-polinsky@users.noreply.github.com>
2024-10-22 14:40:13 -0400

    fix lint setup after ignoring tests

--- packages/oidc-signin-tool/eslint.config.js
+++ packages/oidc-signin-tool/eslint.config.js
@@ -3,14 +3,17 @@ const iTwinPlugin = require("@itwin/eslint-plugin");
 module.exports = [
   {
     files: ["**/*.{ts,tsx}"],
+    ignores: ["**/test-integration/**", "**/test/**"],
     ...iTwinPlugin.configs.iTwinjsRecommendedConfig,
   },
   {
     files: ["**/*.{ts,tsx}"],
+    ignores: ["**/test-integration/**", "**/test/**"],
     ...iTwinPlugin.configs.jsdocConfig,
   },
   {
     files: ["**/*.{ts,tsx}"],
+    ignores: ["**/test-integration/**", "**/test/**"],
     rules: {
       "no-duplicate-imports": "off",
       "@typescript-eslint/consistent-type-imports": "error",
