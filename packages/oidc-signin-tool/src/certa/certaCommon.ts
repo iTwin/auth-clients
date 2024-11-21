ed9fb1256dd62bdd13a22b790df87bae951575e1
Frank Li <53418357+franklbh@users.noreply.github.com>
2024-11-21 08:00:21 -0800

    Enhanced Backend Functionality for Service Authorization (#274)
    Co-authored-by: Nam Le <hoangnam.le@bentley.com>

--- packages/oidc-signin-tool/src/certa/certaCommon.ts
+++ packages/oidc-signin-tool/src/certa/certaCommon.ts
@@ -4,14 +4,24 @@
 *--------------------------------------------------------------------------------------------*/
 import { executeBackendCallback } from "@itwin/certa/lib/utils/CallbackUtils";
 import type { TestBrowserAuthorizationClientConfiguration, TestUserCredentials } from "../TestUsers";
+import type { ServiceAuthorizationClientConfiguration } from "@itwin/service-authorization";
 
 // Shared by both the frontend and backend side of the tests
 export const getTokenCallbackName = "getToken";
+export const getServiceAuthTokenCallbackName = "getServiceAuthToken";
 
 /**
  * Retrieves an access token from the backend using the specified user credentials.
  */
 export async function getAccessTokenFromBackend(user: TestUserCredentials, oidcConfig?: TestBrowserAuthorizationClientConfiguration): Promise<string> {
   const accessToken = await executeBackendCallback(getTokenCallbackName, user, oidcConfig);
+  return accessToken;
+}
+
+/**
+ * Retrieves a service authorization token from the backend.
+ */
+export async function getServiceAuthTokenFromBackend(oidcConfig: ServiceAuthorizationClientConfiguration): Promise<string> {
+  const accessToken = await executeBackendCallback(getServiceAuthTokenCallbackName, oidcConfig);
   return accessToken;
 }
