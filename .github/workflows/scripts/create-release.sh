3b86b1fa0788ad95befc19403512b2ebe74f07fa
anmolshres98 <--local>
2025-01-15 14:31:32 -0600

    hard-code

--- .github/workflows/scripts/create-release.sh
+++ .github/workflows/scripts/create-release.sh
@@ -8,6 +8,9 @@ fi
 refName=$1
 echo "Ref name passed in: $refName"
 
+# TODO: remove this line when ready to merge
+refName="@itwin/service-authorization_v1.2.3"
+
 tagName=$(echo $refName | sed 's/refs\/tags\///')
 echo "Tag name was parsed as: $tagName"
 packageName=$(echo $tagName | sed 's/_v.*//')
