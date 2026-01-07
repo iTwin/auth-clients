e67e28fd1283edbfa7e5aa2d805dec08387a5af5
Alex Dunae <alex.dunae@bentley.com>
2026-01-07 09:38:21 -0800

    .

--- packages/electron/src/test/MainAuthorizationClient.test.ts
+++ packages/electron/src/test/MainAuthorizationClient.test.ts
@@ -1,7 +1,7 @@
 /*---------------------------------------------------------------------------------------------
- * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
- * See LICENSE.md in the project root for license terms and full copyright notice.
- *--------------------------------------------------------------------------------------------*/
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
 
 import type { AuthorizationServiceConfiguration, TokenRequest } from "@openid/appauth";
 import { BaseTokenRequestHandler } from "@openid/appauth";
