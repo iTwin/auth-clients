d696ed238ffacadccded1c1728859990ad39366e
Ben Polinsky <ben-polinsky@users.noreply.github.com>
2026-02-03 10:37:53 -0500

    clean up

--- packages/browser/src/integration-tests/test-app/silent-renew.ts
+++ packages/browser/src/integration-tests/test-app/silent-renew.ts
@@ -4,12 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { BrowserAuthorizationClient } from "../../Client";
-
-// import { UserManager } from "oidc-client-ts";
-
 // // This page handles the silent renew callback in an iframe
 // // It's intentionally minimal to load quickly
-// // We use signinSilentCallback directly because BrowserAuthorizationClient.handleSignInCallback
-// // checks against redirectUri, not silentRedirectUri
-// new UserManager({ authority: "", client_id: "" }).signinSilentCallback();
+
 BrowserAuthorizationClient.handleSignInCallback();
